ca7e8656e080f31217b62e3b2ca5d675d8032b8c
adamkrieger <adamkrieger.ca@gmail.com>
2014-08-03 23:43:01 -0500

    Implemented starter code for outputting built site.

--- Program.fs
+++ Program.fs
@@ -1,11 +1,30 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open System
+open System.IO
+open System.Configuration
+
+let createDirectoryIfItDoesNotExist (dir)=
+    match Directory.Exists(dir) with
+    | false -> Directory.CreateDirectory(dir) |> ignore
+    | _ -> None |> ignore
+    dir
+
+let addOutputDirectorySuffix (x)= x + "_bin\\"
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
 
-    use fs = new System.IO.StreamWriter(".\index.html")
+    let root = ConfigurationManager.AppSettings.Item("rootDirectory") |>
+                createDirectoryIfItDoesNotExist
+
+    let output = root |> 
+                    addOutputDirectorySuffix |> 
+                    createDirectoryIfItDoesNotExist
+    
+    use fs = new System.IO.StreamWriter(output + "index.html")
 
     fs.Write("<html></html>")
 
