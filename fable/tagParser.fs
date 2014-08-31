74a3ae1b227d23cad83b0cd2d86b15a8afdf0d1d
adamkrieger <adamkrieger.ca@gmail.com>
2014-08-30 21:24:16 -0500

    Completed the first round of the fable tag parser.

---
+++ fable/tagParser.fs
@@ -0,0 +1,50 @@
+﻿namespace fable
+
+open FParsec.CharParsers
+open FParsec
+open System
+
+type TagParser(content:string) =
+
+    let contentTag =
+        pstring "content"
+        
+    let fableTagChoice = 
+        [
+            contentTag |>> (fun tag -> content)
+        ]
+        |> choice
+
+    let startTag =
+        pstring "[#"
+
+    let endTag = 
+        pstring "#]"
+
+    let fableTag = 
+        startTag >>. spaces >>. (fableTagChoice) .>> spaces .>> endTag
+
+    let parseUntilTag =
+        charsTillString "[#" false 100
+
+    let parseRouting fableParser =
+        attempt fableParser <|>
+        attempt parseUntilTag <|>
+        restOfLine true
+
+    //overhead functions for test purposes
+    let resolveResult result =
+        match result with
+        | Success(result,_,_) -> result
+        | Failure(errorMsg,_,_) -> raise(Exception(errorMsg))
+
+//    let dummyTest =
+//        let x = resolveResult (run startTag "[#")
+//        resolveResult (run (compile "") "")
+
+//    let test parser input =
+//        resolveResult (run parser input)
+
+    member this.compile =
+        let fableParser = fableTagChoice
+        manyTill (parseRouting fableParser) eof
