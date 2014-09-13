adf157e9a244f0433228316a421a506247794e1e
adamkrieger <adamkrieger.ca@gmail.com>
2014-09-13 09:13:43 -0500

    Added horizontal rule parser to CommonMarkParser.

--- fable.tests/CommonMark.Tests.fs
+++ fable.tests/CommonMark.Tests.fs
@@ -13,3 +13,18 @@ type ``When the content contains tabs`` () =
     [<Test>] member x.
      ``The tabs should be replaced with four spaces`` () =
         output |> should equal "This is some     content\nAnd    some more.\n"
+
+[<TestFixture>]
+type ``When parsing a leaf block`` () =
+
+    let parser = new CommonMarkParser()
+
+//    A line consisting of 0-3 spaces of indentation, 
+//    followed by a sequence of three or more matching -, _, or * characters, 
+//    each followed optionally by any number of spaces, forms a horizontal rule.
+
+    [<Test>] member x.
+     ``Zero spaces and three underscores results in a horizontal rule`` () =
+        "___"
+            |> parser.compile
+            |> should equal "<hr />"
--- fable/CommonMarkParser.fs
+++ fable/CommonMarkParser.fs
@@ -5,20 +5,54 @@ open ParserHelpers
 
 type CommonMarkParser () =
 
+    let iterator parser = manyTill parser eof
+
+    let zeroToThreeSpaces = 
+        [
+            pstring "   "
+            pstring "  "
+            pstring " "
+            pstring ""
+        ]
+        |> choice
+
+    let horizontalRuleSequence =
+        [
+            pstring "---"
+            pstring "___"
+            pstring "***"
+        ]
+        |> choice
+
     let preprocessor input =
         let tabToSpacesParser = 
             [
                 attempt (tab |>> (fun x -> "    "))
                 anyChar |>> (fun x -> x.ToString())
             ]
-            |> choice 
+            |> choice
+            |> iterator
 
-        let iterator = manyTill tabToSpacesParser eof
+        (run tabToSpacesParser input) 
+            |> resolveResult
 
-        (run iterator input) |> resolveResult
+    let horizontalRule input =
+        let horizontalRuleParser =
+            [
+                attempt zeroToThreeSpaces >>. horizontalRuleSequence .>> restOfLine true
+                    |>> (fun x -> "<hr />")
+                restOfLine true
+            ]
+            |> choice
+            |> iterator
+
+        (run horizontalRuleParser input)
+            |> resolveResult
 
     member this.compile input = 
         let res = preprocessor input |> List.reduce (fun a b -> a + b)
+
+        let res = horizontalRule res |> List.reduce (fun a b -> a + b)
 
         res
 
